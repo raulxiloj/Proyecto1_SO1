4803b014d100818a5a0e5149f2a8a1db42c0d29f
raulxiloj <raulxiloj98@gmail.com>
2020-12-17 21:38:44 -0600

    Se creo otro endpoint con websocket para mandar la informacion del porcentaje de utilizacion del cpu

--- server/main.go
+++ server/main.go
@@ -5,6 +5,8 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/gorilla/websocket"
@@ -49,6 +51,57 @@ func reader(ws *websocket.Conn) {
 	}
 }
 
+func getCPU() (idle, total uint64) {
+	contents, err := ioutil.ReadFile("/proc/stat")
+	if err != nil {
+		return
+	}
+	lines := strings.Split(string(contents), "\n")
+	for _, line := range lines {
+		fields := strings.Fields(line)
+		if fields[0] == "cpu" {
+			numFields := len(fields)
+			for i := 1; i < numFields; i++ {
+				val, err := strconv.ParseUint(fields[i], 10, 64)
+				if err != nil {
+					fmt.Println("Error: ", i, fields[i], err)
+				}
+				total += val //tally up all the numbers to get total ticks
+				if i == 4 {
+					idle = val
+				}
+			}
+			return
+		}
+	}
+	return
+}
+
+func readerCPU(ws *websocket.Conn) {
+	for { //for loop that run forever (like a while(true))
+
+		messageType, _, err := ws.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		for {
+			idle0, total0 := getCPU()
+			time.Sleep(1 * time.Second)
+			idle1, total1 := getCPU()
+
+			idleTicks := float64(idle1 - idle0)
+			totalTicks := float64(total1 - total0)
+			cpuUsage := 100 * (totalTicks - idleTicks) / totalTicks
+			fmt.Printf("CPU usage is %f%% ", cpuUsage)
+			ws.WriteMessage(messageType, []byte(strconv.FormatFloat(cpuUsage, 'f', 2, 64)))
+			time.Sleep(1 * time.Second)
+		}
+
+	}
+}
+
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to my Socket Server")
 }
@@ -63,9 +116,20 @@ func wsHandler(w http.ResponseWriter, r *http.Request) {
 	reader(ws) //Listen indefinitely for new messages coming through on our ws connection
 }
 
+func cpuHandler(w http.ResponseWriter, r *http.Request) {
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	log.Println("Client Succesfully connected to cpu socket")
+	readerCPU(ws)
+}
+
 func main() {
 	http.HandleFunc("/", homePage)
 	http.HandleFunc("/ws", wsHandler)
+	http.HandleFunc("/ws2", cpuHandler)
 	log.Println("Server on port :8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
